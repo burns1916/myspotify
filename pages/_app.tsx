99bcbff406194391d91b91f00be6d52557b8482b
Ryan Burns <burns@ryans-air.attlocal.net>
2022-04-06 19:32:43 -0700

    created page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import PlayerLayout from '../components/playerLayout';
 import 'reset-css';
 
 const theme = extendTheme({
@@ -33,7 +34,9 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return(
   <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
+    <PlayerLayout>
+    <Component {...pageProps} />;
+    </PlayerLayout>
   </ChakraProvider>
   )
 }
