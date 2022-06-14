90089a005e0729253bb0a8f0ae607deb6afc65d7
Ryan Burns <burns@ryans-air.attlocal.net>
2022-06-14 10:32:33 -0700

    added gradient layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import GradientLayout from "../components/gradientLayout";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout color="red">
+      <div>Homepage</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
